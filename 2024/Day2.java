4e3c76a3b04e0c183872e84087fb6f78ba741538
Yasmin Woodlock <yasminwoodlock@gmail.com>
2024-12-02 11:54:42 +0000

    Day2 done

--- 2024/Day2.java
+++ 2024/Day2.java
@@ -1,78 +1,71 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Day2 {
-    public static boolean decreasing(String[] arr){
-        boolean dec = false;
-        for(int i=1; i<arr.length; i++){
-            if(Integer.parseInt(arr[i-1]) - Integer.parseInt(arr[i]) > 0){
-                dec = true;
-            }
-            else{
+    public static boolean safe1(List<Integer> levels) {
+        // Check the condition: differences between consecutive elements are between 1 and 3
+        for (int i = 0; i < levels.size() - 1; i++) {
+            int diff = Math.abs(levels.get(i) - levels.get(i + 1));
+            if (diff < 1 || diff > 3) {
                 return false;
             }
         }
-        return dec;
+
+        // Check if levels are sorted in ascending or descending order
+        List<Integer> sorted = new ArrayList<>(levels);
+        Collections.sort(sorted);
+        List<Integer> reversed = new ArrayList<>(sorted);
+        Collections.reverse(reversed);
+
+        return levels.equals(sorted) || levels.equals(reversed);
     }
-    public static boolean increasing(String[] arr){
-        boolean inc = false;
-        for(int i=1; i<arr.length; i++){
-            if(Integer.parseInt(arr[i]) - Integer.parseInt(arr[i-1]) > 0){
-                inc = true;
-            }
-            else{
-                return false;
+
+    public static boolean safe2(List<Integer> levels) {
+        for (int i = 0; i < levels.size(); i++) {
+            List<Integer> modifiedArray = new ArrayList<>(levels);
+            modifiedArray.remove(i);
+            if (safe1(modifiedArray)) {
+                return true;
             }
         }
-        return inc;
-    }
-    public static boolean isSafe(String[] arr){
-        int diff = 0;
-            for(int i=1; i<arr.length; i++){
-                int prob1 = Integer.parseInt(arr[i]);
-                int prob2 = Integer.parseInt(arr[i-1]);
-                if(Math.abs(prob2 - prob1) > diff){
-                    diff = Math.abs(prob2 - prob1);
-                }
-            }
-
-        if(diff > 0 && diff < 4){
-            return true;
-        }
-
         return false;
     }
-    public static void main(String[] args) {
-        ArrayList<String[]> ls = new ArrayList<>();
-        try {
-            File myObj = new File("text files/Day2");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                String[] temp = data.split(" ");
-                ls.add(temp);
-            }
-            myReader.close();
-//            for(String[] i : ls){
-//                System.out.println(Arrays.toString(i));
-//            }
-        }catch(FileNotFoundException e){
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
 
-        int safe = 0;
-        for(String[] arr : ls){
-            if(increasing(arr) || decreasing(arr)){
-                if(isSafe(arr)){
-                    safe ++;
+    public static void main(String[] args) {
+        try {
+            // Read input from the file
+            List<List<Integer>> ls = new ArrayList<>();
+            BufferedReader br = new BufferedReader(new FileReader("text files/Day2"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                List<Integer> numbers = new ArrayList<>();
+                String[] temp = line.split(" ");
+                for(String i : temp){
+                    numbers.add(Integer.parseInt(i));
+                }
+                ls.add(numbers);
+            }
+            br.close();
+
+            int part1 = 0;
+            for(List<Integer> arr : ls){
+                if(safe1(arr)){
+                    part1 ++;
                 }
             }
-        }
 
-        System.out.println(safe);
+            System.out.println(part1);
+
+            int part2 = 0;
+            for(List<Integer> arr : ls){
+                if(safe2(arr)){
+                    part2++;
+                }
+            }
+            System.out.println(part2);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
