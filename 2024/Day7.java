178a638c267347fde779d31cf05fa4d1a4e9c66c
Yasmin Woodlock <yasminwoodlock@gmail.com>
2024-12-07 12:50:53 +0000

    Day 7 Done

--- 2024/Day7.java
+++ 2024/Day7.java
@@ -4,10 +4,27 @@ import java.util.Scanner;
 
 public class Day7 {
 
+    public static long concat(long number1, long number2){
+        String temp = Long.toString(number1) + Long.toString(number2);
+        return Long.parseLong(temp);
+    }
+
+    public static boolean evaluateCombinations1(long[] numbers, int index, long currentValue, long targetValue) {
+        if (index == numbers.length - 1) {
+            return currentValue == targetValue;
+        }else if (evaluateCombinations(numbers, index+1, concat(currentValue, numbers[index+1]), targetValue)){
+            return true;
+        }
+        else return evaluateCombinations(numbers, index + 1, currentValue * numbers[index + 1], targetValue);
+    }
+
+
     public static boolean evaluateCombinations(long[] numbers, int index, long currentValue, long targetValue) {
         if (index == numbers.length - 1) {
             return currentValue == targetValue;
         } else if (evaluateCombinations(numbers, index + 1, currentValue + numbers[index + 1], targetValue)) {
+            return true;
+        } else if (evaluateCombinations(numbers, index+1, concat(currentValue, numbers[index+1]), targetValue)){
             return true;
         }
         else return evaluateCombinations(numbers, index + 1, currentValue * numbers[index + 1], targetValue);
@@ -17,8 +34,15 @@ public class Day7 {
         return evaluateCombinations(numbers, 0, numbers[0], target);
     }
 
+    public static boolean trueStatement1(long target, long[] numbers){
+        return evaluateCombinations1(numbers, 0, numbers[0], target);
+    }
+
+
     public static void main(String[] args) {
-        long totalCalculation = 0;
+        long totalCalculation1 = 0;
+        long totalCalculation2 = 0;
+
         try{
             Scanner input = new Scanner(Paths.get("text files/Day7"));
 
@@ -33,12 +57,17 @@ public class Day7 {
                     numbers[i] = Long.parseLong(numberString[i]);
                 }
 
+                if(trueStatement1(targetValue, numbers)){
+                    totalCalculation1 += targetValue;
+                }
+
                 if(trueStatement(targetValue, numbers)){
-                    totalCalculation += targetValue;
+                    totalCalculation2 += targetValue;
                 }
             }
 
-            System.out.println(totalCalculation);
+            System.out.println("Part 1: " + totalCalculation1);
+            System.out.println("Part 2: " + totalCalculation2);
         } catch (IOException e){
             System.out.println(e.getMessage());
         }
